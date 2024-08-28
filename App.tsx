757e9fd46a58d14b625ef1a0f26b3d606ae079c4
Yassine Drafate <yassinedrafate1@gmail.com>
2024-08-28 15:07:07 +0100

    installing necessary libraries

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Scractch from "./Scratch";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <>
+      {/* <Scractch /> */}
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ Scratch.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from "@/components/ui/spinner";
+
+const Scractch = () => {
+  return (
+    <>
+      <Spinner />
+    </>
+  );
+};
+
+export default Scractch;
